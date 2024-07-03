4f54b29af10710d0358213029d61d6b64046ec1e
Sarathlal shaji <143636178+polisarath@users.noreply.github.com>
2024-07-03 19:05:11 +0530

    Update payment.service.js

--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -22,7 +22,7 @@ const createPaymentLink= async (orderId)=>{
             email: true,
           },
           reminder_enable: true,
-          callback_url: `http://localhost:3001/payment/${orderId}`,
+          callback_url: `https://fashion-ecom.onrender.com//${orderId}`,
           callback_method: 'get',
         };
     
