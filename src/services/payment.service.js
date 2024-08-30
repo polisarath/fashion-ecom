3a22c52ebd559df00df0f5a1d34f20c6ee313252
Sarathlal shaji <143636178+polisarath@users.noreply.github.com>
2024-08-30 08:56:51 +0530

    Update payment.service.js
    domain changed

--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -22,7 +22,7 @@ const createPaymentLink= async (orderId)=>{
             email: true,
           },
           reminder_enable: true,
-          callback_url: `https://e-commerce-myproject.vercel.app/payment/${orderId}`,
+          callback_url: `https://www.shopwithsara.shop/payment/${orderId}`,
           callback_method: 'get',
         };
     
